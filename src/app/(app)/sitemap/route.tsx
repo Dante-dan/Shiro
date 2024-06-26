1b6f9ab6898fca43278c513a8dbf8d6f7a612e29
duanjl <duanjl.china@gmail.com>
2024-06-26 19:28:12 +0800

    fix: add static url to sitemap

--- src/app/(app)/sitemap/route.tsx
+++ src/app/(app)/sitemap/route.tsx
@@ -13,6 +13,58 @@ export const GET = async () => {
       return fetch(path).then((res) => res.text())
     },
   })
+  data.replace(
+    '<url>',
+    `
+  <url>
+    <loc>https://blog.dhpie.com/</loc>
+    <lastmod>2024-06-26T11:22:30+00:00</lastmod>
+    <priority>1.00</priority>
+    <changefreq>daily</changefreq>
+  </url>
+  <url>
+  <loc>https://blog.dhpie.com/timeline?type=note</loc>
+  <lastmod>2024-06-26T11:22:30+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://blog.dhpie.com/posts</loc>
+  <lastmod>2024-06-26T11:22:30+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://blog.dhpie.com/notes</loc>
+  <lastmod>2024-06-26T11:22:30+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://blog.dhpie.com/timeline</loc>
+  <lastmod>2024-06-26T11:22:30+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://blog.dhpie.com/friends</loc>
+  <lastmod>2024-06-26T11:22:30+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://blog.dhpie.com/thinking</loc>
+  <lastmod>2024-06-26T11:22:30+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://blog.dhpie.com/projects</loc>
+  <lastmod>2024-06-26T11:22:30+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://blog.dhpie.com/says</loc>
+  <lastmod>2024-06-26T11:22:30+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  `,
+  )
 
   return new Response(data, {
     headers: {
