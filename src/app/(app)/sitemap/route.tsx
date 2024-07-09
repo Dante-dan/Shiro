1a1dcd7e51935197cff272f8cbe4b847e7b3816e
duanjl <duanjl.china@gmail.com>
2024-07-09 14:52:08 +0800

    fix: add static url to sitemap

--- src/app/(app)/sitemap/route.tsx
+++ src/app/(app)/sitemap/route.tsx
@@ -6,14 +6,14 @@ export const revalidate = 3600 // 1 hour
 export const GET = async () => {
   const queryClient = getQueryClient()
 
-  const data = await queryClient.fetchQuery({
+  let data = await queryClient.fetchQuery({
     queryKey: ['sitemap'],
     queryFn: async () => {
       const path = apiClient.aggregate.proxy.sitemap.toString(true)
       return fetch(path).then((res) => res.text())
     },
   })
-  data.replace(
+  data = data.replace(
     '<url>',
     `
   <url>
